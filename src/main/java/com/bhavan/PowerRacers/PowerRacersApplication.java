827b30c58541e49ea7b07add3626ee6b646fb50c
Bhavan Sai <91448593+BhavanSai12@users.noreply.github.com>
2023-02-21 11:00:58 +0530

    Project upload without anything

---
+++ src/main/java/com/bhavan/PowerRacers/PowerRacersApplication.java
@@ -0,0 +1,13 @@
+package com.bhavan.PowerRacers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerRacersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerRacersApplication.class, args);
+	}
+
+}
